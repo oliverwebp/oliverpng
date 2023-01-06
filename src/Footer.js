afaa7feae4177e716698e66da21dff41f7b5f502
oliverwebp <119653688+oliverwebp@users.noreply.github.com>
2023-01-05 17:52:34 -0800

    error in savefixed

--- src/Footer.js
+++ src/Footer.js
@@ -6,25 +6,16 @@ import instagramlogo from './instagramlogo.png';
 function Footer() {
     return (
         <footer className='MainFooter'>
-            <ul className='inside'>
-                <li>
-                    <a href='https://github.com/oliverwebp' target='_blank' rel='noreferrer'><img className='icons' src={githublogo} alt='Github Logo'/></a>
-                </li>
-                <li>
-                    <a href='https://www.linkedin.com/in/oliver-nguyen-74746719b/' target='_blank' rel='noreferrer'><img className='icons' src={linkedinlogo} alt='LinkedIn Logo' /></a>
-                </li>
-                <li>
-                    <a href='https://www.instagram.com/ollie.ngu/' target='_blank' rel='noreferrer'><img className='icons' src={instagramlogo} alt='Instagram Logo' /></a>
-                </li>
-            </ul>
-            <ul className='outside'>
-                <li>
-                    <p className='copyright'>	&#169;</p>
-                </li>
-                <li>
-                    <h1>2022 Oliver Nguyen. All Rights Reserved</h1>
-                </li>
-            </ul>
+            <div className='inside'>
+                <a href='https://github.com/oliverwebp' target='_blank' rel='noreferrer'><img className='icons' src={githublogo} alt='Github Logo'/></a>
+                <a href='https://www.linkedin.com/in/oliver-nguyen-74746719b/' target='_blank' rel='noreferrer'><img className='icons' src={linkedinlogo} alt='LinkedIn Logo' /></a>
+                <a href='https://www.instagram.com/ollie.ngu/' target='_blank' rel='noreferrer'><img className='icons' src={instagramlogo} alt='Instagram Logo' /></a>
+
+            </div>
+            <div className='outside'>
+                <p className='copyright'> &#169;</p>
+                <h1>2022 Oliver Nguyen. All Rights Reserved.</h1>
+            </div>
         </footer>
     )
 }
