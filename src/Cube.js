7550a571617745c9521342e99ca7d2dc36e9d841
oliverwebp <119653688+oliverwebp@users.noreply.github.com>
2023-12-07 11:55:57 -0800

    clean  upp

--- src/Cube.js
+++ src/Cube.js
@@ -1,11 +1,9 @@
 import * as THREE from "three";
-import rubik_cub from './rubiks_cube.glb';
-import React, { Suspense, useLayoutEffect, useEffect, useRef } from "react";
+import React, {} from "react";
 import './Cube.css'
-import Model2 from "./Model2?url";
-import Model3 from "./Model3?url";
-import {Canvas, useFrame } from '@react-three/fiber'
-import {Html, ScrollControls, useScroll, useGLTF, useAnimations, OrbitControls,
+import Model from "./Model?url";
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls,
      //useFBX
      } from "@react-three/drei";
 
@@ -13,21 +11,13 @@ import {Html, ScrollControls, useScroll, useGLTF, useAnimations, OrbitControls,
 
 
 const Cube = () => {
-    
-  /*function Moyu({...props}) {
-    const scroll = useScroll()
-    const { scene, nodes, animations } = useGLTF(rubik_cub)
-
-    return <primitive object={scene} {...props} /> 
-  }
-  */
 return (
     <div className="contCu">
         <Canvas camera={{ near: 1, far: 800, position: [50, 50, 50] }}>
             <color attach="background" args={['#e1dfdd']} />
             <OrbitControls enableZoom={false}></OrbitControls>
-            <Model2></Model2>
-            <ambientLight ></ambientLight>
+            <Model scale={.4}></Model>
+            <ambientLight intensity={4}></ambientLight>
             <pointLight position={[10, 10, 10]} />
 
         </Canvas>
